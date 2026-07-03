bc8799c0e0c82a66edc4117fd1d1416f26c370dc
Vishal Purushothaman <vega1four@gmail.com>
2026-07-03 15:46:19 +0530

    Enhance console output with pizza messages
    Added additional console output statements about pizza.

--- day01-introduction-to-java/src/Main.java
+++ day01-introduction-to-java/src/Main.java
@@ -1,8 +1,19 @@
+/*
+ * Day 01 - Introduction to Java
+ * Main.java
+ *
+ * This program demonstrates the basic structure
+ * of a Java program and console output.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, Java!");
+        // Printing text to the console
+        System.out.println("I like pizza");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
 
     }
 
