4fb5365ab1e7b8d7793a429140be4a445b69d5b5
Vishal Purushothaman <vega1four@gmail.com>
2026-07-10 13:56:49 +0530

    Implement arithmetic operations and conditionals in Practice.java

--- day05-arithmetic/src/Practice.java
+++ day05-arithmetic/src/Practice.java
@@ -2,7 +2,53 @@ public class Practice {
 
     public static void main(String[] args) {
 
-        // Write your practice code here
+        int number = 10;
+
+        System.out.println("Initial Value : " + number);
+
+        number++;
+
+        System.out.println("After ++ : " + number);
+
+        number--;
+
+        System.out.println("After -- : " + number);
+
+        number += 15;
+
+        System.out.println("After += 15 : " + number);
+
+        number -= 5;
+
+        System.out.println("After -= 5 : " + number);
+
+        number *= 2;
+
+        System.out.println("After *= 2 : " + number);
+
+        number /= 4;
+
+        System.out.println("After /= 4 : " + number);
+
+        number %= 3;
+
+        System.out.println("After %= 3 : " + number);
+
+        System.out.println();
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println("PEMDAS Result : " + result);
+
+        System.out.println();
+
+        int evenNumber = 24;
+
+        if (evenNumber % 2 == 0) {
+            System.out.println(evenNumber + " is Even");
+        } else {
+            System.out.println(evenNumber + " is Odd");
+        }
 
     }
 
